0753b6c03c3c7a378ee10a68ae9bce803ad50000
googl <Uglyanitsa_M@it.belstu.by>
2023-10-25 01:36:51 +0300

    Laboratory work 09

---
+++ 09_Коллекции/lab09/InternetResource.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace lab09
+{
+    public class InternetResource : IList
+    {
+        private List<object> pages = new List<object>();
+        public struct ResourceInfo
+        {
+            public string Name { get; private set; }
+            public string Domain { get; private set; }
+            public string Description { get; private set; }
+            public string MetaData { get; private set; }
+
+            public ResourceInfo(string name, string domain, string description, string metaData)
+            {
+                Name = name;
+                Domain = domain;
+                Description = description;
+                MetaData = metaData;
+            }
+        }
+        public ResourceInfo Resource { get; set; }
+
+        public InternetResource(string name, string domain, string description, string metaData) 
+        {
+            Resource = new ResourceInfo(name, domain, description, metaData);
+        }
+
+        public void AddPage(string page)
+        {
+            pages.Add(page);
+        }
+
+        public void RemovePage(string page)
+        {
+            pages.Remove(page);
+        }
+
+        public int Add(object value)
+        {
+            pages.Add(value);
+            return pages.Count - 1;
+        }
+
+        public bool Contains(object value)
+        {
+            return pages.Contains(value);
+        }
+
+        public void Clear()
+        {
+            pages.Clear();
+        }
+
+        public int IndexOf(object value)
+        {
+            return pages.IndexOf(value);
+        }
+
+        public void Insert(int index, object value)
+        {
+            pages.Insert(index, value);
+        }
+
+        public void Remove(object value)
+        {
+            pages.Remove(value);
+        }
+
+        public void RemoveAt(int index)
+        {
+            pages.RemoveAt(index);
+        }
+
+        public void CopyTo(Array array, int index)
+        {
+            pages.CopyTo((object[])array, index);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            return pages.GetEnumerator();
+        }
+
+        public object this[int index]
+        {
+            get { return pages[index]; }
+            set { pages[index] = value; }
+        }
+
+        public bool IsReadOnly => false;
+
+        public bool IsFixedSize => false;
+
+        public int Count => pages.Count;
+
+        public object SyncRoot => null;
+
+        public bool IsSynchronized => false;
+    }
+}
---
+++ 09_Коллекции/lab09/Program.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+
+namespace lab09
+{
+    internal class Program
+    {
+        public static void FirstTask()
+        {
+            Console.WriteLine("\t\tПервое задание");
+
+            List<InternetResource> resourceList = new List<InternetResource>();
+
+            InternetResource resource1 = new InternetResource("Resource 1", "example.com", "Description 1", "Metadata 1");
+            InternetResource resource2 = new InternetResource("Resource 2", "sample.com", "Description 2", "Metadata 2");
+            InternetResource resource3 = new InternetResource("Resource 3", "test.com", "Description 3", "Metadata 3");
+            InternetResource resource4 = new InternetResource("Resource 4", "demo.com", "Description 4", "Metadata 4");
+
+            resource1.AddPage("Page 1");
+            resource1.AddPage("Page 2");
+            resource2.AddPage("Page A");
+            resource2.AddPage("Page B");
+            resource3.AddPage("Page X");
+            resource3.AddPage("Page Y");
+            resource4.AddPage("Page Z");
+            resource4.AddPage("Page W");
+
+            resourceList.Add(resource1);
+            resourceList.Add(resource2);
+            resourceList.Add(resource3);
+            resourceList.Add(resource4);
+
+            foreach (InternetResource resource in resourceList)
+            {
+                Console.WriteLine($"Имя ресурса: {resource.Resource.Name}");
+                Console.WriteLine("Страницы ресурса:");
+                foreach (string page in resource)
+                {
+                    Console.WriteLine(page);
+                }
+            }
+        }
+
+        public static void SecondTask()
+        {
+            Console.WriteLine("\t\tВторое задание");
+
+            ConcurrentQueue<int> collection = new ConcurrentQueue<int>();
+            for (int i = 1; i <= 10; i++)
+            {
+                collection.Enqueue(i);
+            }
+
+            int n = 3;
+            for (int i = 0; i < n; i++)
+            {
+                int removedItem;
+                if (collection.TryDequeue(out removedItem))
+                {
+                    Console.WriteLine($"Удалён: {removedItem}");
+                }
+            }
+
+            for (int i = 11; i <= 15; i++)
+            {
+                collection.Enqueue(i);
+            }
+
+            List<int> secondCollection = new List<int>(collection);
+
+            Console.WriteLine("Вторая коллекция:");
+            foreach (int item in secondCollection)
+            {
+                Console.WriteLine(item);
+            }
+
+            int targetValue = 8;
+            if (secondCollection.Contains(targetValue))
+            {
+                Console.WriteLine($"Значение {targetValue} найдено в другой коллекции.");
+            }
+            else
+            {
+                Console.WriteLine($"Значение {targetValue} не найдено в другой коллекции.");
+            }
+        }
+
+        public static void ThirdTask()
+        {
+            Console.WriteLine("\t\tТретье задание");
+
+            ObservableCollection<InternetResource> resourceCollection = new ObservableCollection<InternetResource>();
+
+            resourceCollection.CollectionChanged += ResourceCollectionChanged;
+
+            InternetResource resource1 = new InternetResource("Resource 1", "example.com", "Description 1", "Metadata 1");
+            InternetResource resource2 = new InternetResource("Resource 2", "sample.com", "Description 2", "Metadata 2");
+
+            resourceCollection.Add(resource1);
+            resourceCollection.Add(resource2);
+
+            Console.WriteLine("\nКоллекция:");
+            PrintResourceCollection(resourceCollection);
+            Console.WriteLine();
+
+            InternetResource resource3 = new InternetResource("Resource 3", "test.com", "Description 3", "Metadata 3");
+            resourceCollection.Add(resource3);
+
+            Console.WriteLine("\nКоллекция после добавления Resource 3:");
+            PrintResourceCollection(resourceCollection);
+            Console.WriteLine();
+
+            resourceCollection.RemoveAt(1);
+
+            Console.WriteLine("\nКоллекция после удаления Resource 2:");
+            PrintResourceCollection(resourceCollection);
+        }
+
+        private static void ResourceCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            if (e.Action == NotifyCollectionChangedAction.Add)
+            {
+                Console.WriteLine("Элементы добавлены в коллекцию.");
+            }
+            else if (e.Action == NotifyCollectionChangedAction.Remove)
+            {
+                Console.WriteLine("Элементы удалены из коллекции.");
+            }
+        }
+
+        private static void PrintResourceCollection(ObservableCollection<InternetResource> collection)
+        {
+            foreach (InternetResource resource in collection)
+            {
+                Console.WriteLine($"Имя ресурса: {resource.Resource.Name}");
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            FirstTask();
+            SecondTask();
+            ThirdTask();
+        }
+    }
+}
