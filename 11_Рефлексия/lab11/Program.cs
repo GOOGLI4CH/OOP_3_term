205dd578fdb30922a999eb0a049fc1cedc0f5e82
googl <Uglyanitsa_M@it.belstu.by>
2023-11-29 17:45:06 +0300

    Laboratory work 11

---
+++ 11_Рефлексия/lab11/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace lab11
+{
+    public class Program
+    {
+        static void AnalyzeClass(string className)
+        {
+            var type = Type.GetType(className);
+
+            Console.WriteLine($"Класс: {className}");
+            Console.WriteLine($"Имя сборки: {Reflector.GetAssemblyName(type)}");
+            Console.WriteLine($"Наличие публичных конструкторов: {Reflector.HasPublicConstructors(type)}");
+            Console.WriteLine($"Названия методов: {String.Join(Environment.NewLine, Reflector.GetMethodsNames(type))}");
+            Console.WriteLine($"Названия методов с определёнными параметрами: {Reflector.GetMethodsByParamType(type, typeof(object))}");
+            Console.WriteLine($"Названия полей и свойств: {String.Join(Environment.NewLine, Reflector.GetFieldsAndPropertiesNames(type))}");
+            Console.WriteLine($"Названия интерфейсов: {String.Join(Environment.NewLine, Reflector.GetInterfacesNames(type))}");
+
+            Reflector.WriteResultsTofile(className, "object.txt");
+        }
+
+        static void Main(string[] args)
+        {
+            AnalyzeClass("System.Object");
+        }
+    }
+}
---
+++ 11_Рефлексия/lab11/Reflector.cs
@@ -0,0 +1,76 @@
+﻿using System.Linq;
+using System.Reflection;
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace lab11
+{
+    public static class Reflector
+    {
+        public static string GetAssemblyName(Type type)
+        {
+            var assembly = type.Assembly;
+
+            return assembly.GetName().Name;
+        }
+
+        public static string HasPublicConstructors(Type type)
+        {
+            var constructors = type.GetConstructors();
+
+            return $"{constructors.Any(c => c.IsPublic)}";
+        }
+
+        public static IEnumerable<string> GetMethodsNames(Type type)
+        {
+            var methods = type.GetMethods(BindingFlags.Public | BindingFlags.Instance);
+            return methods.Select(m => m.Name);
+        }
+
+        public static IEnumerable<string> GetFieldsAndPropertiesNames(Type type)
+        {
+            var fields = GetMembersNames(type.GetFields(BindingFlags.Public | BindingFlags.Instance));
+            var properties = GetMembersNames(type.GetProperties(BindingFlags.Public | BindingFlags.Instance));
+            return fields.Concat(properties);
+        }
+
+        public static IEnumerable<string> GetInterfacesNames(Type type)
+        {
+            return type.GetInterfaces().Select(i => i.FullName);
+        }
+
+        private static IEnumerable<string> GetMembersNames(MemberInfo[] members)
+        {
+            return members.Select(m => m.Name);
+        }
+
+        public static string GetMethodsByParamType(Type type, Type paramType)
+        {
+            var methods = type.GetMethods()
+              .Where(m => m.GetParameters()
+                .Any(p => p.ParameterType == paramType))
+              .Select(m => m.Name);
+
+            return String.Join(Environment.NewLine, methods);
+        }
+
+        public static void WriteResultsTofile(string typeName, string fileName)
+        {
+            var type = Type.GetType(typeName);
+
+            var results =
+              $"Имя сборки: {GetAssemblyName(type)}{Environment.NewLine}" +
+              $"Наличие публичных конструкторов: {HasPublicConstructors(type)}{Environment.NewLine}" +
+              $"Названия методов: {String.Join(Environment.NewLine, GetMethodsNames(type))}{Environment.NewLine}" +
+              $"Названия методов с определёнными параметрами: {Reflector.GetMethodsByParamType(type, typeof(string))}" +
+              $"Названия полей и свойств: {String.Join(Environment.NewLine, GetFieldsAndPropertiesNames(type))}{Environment.NewLine}" +
+              $"Названия интерфейсов: {String.Join(Environment.NewLine, GetInterfacesNames(type))}{Environment.NewLine}";
+
+            using (StreamWriter writer = File.CreateText(fileName))
+            {
+                writer.Write(results);
+            }
+        }
+    }
+}
